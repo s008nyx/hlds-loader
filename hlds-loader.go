703dc0f01855dc448dbb2eb2b1daf51cf995c408
Алексей Митрошкин <aleksey.mitroshkin@webpp.ru>
2021-04-25 07:41:45 +0300

    first commit

---
+++ hlds-loader.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"io"
+)
+
+type Item struct {
+    Name string `json:"name"`
+    Type string `json:"type"`
+    Mtime string `json:"mtime"`
+    Size uint64 `json:"size,omitempty"`
+}
+
+func main() {
+	fmt.Println("----------------------------------------------------")
+	fmt.Println("------------| (Re)HLDS Installer v0.1 |-------------")
+	fmt.Println("----------------------------------------------------")
+
+	routes := make(map[int]string)
+	
+	depth := 0
+	routes[depth] = "http://dl.rehlds.ru"
+
+	do(routes, Item{Name:"",Type:"",Mtime: ""}, depth)
+}
+
+
+func do(routes map[int]string, item Item, depth int) {
+	if isFile(item) {
+		err := load(routes, item, depth)
+		if err == nil {
+			if depth <= 1 {
+				do(routes, Item{Name:"",Type:"",Mtime: ""}, 0)
+			} else {
+				do(routes, Item{Name:"",Type:"",Mtime: ""}, depth - 1)
+			}
+		}
+	} else {
+		list(routes, item, depth)
+	}
+}
+
+func getUrl(routes map[int]string, item Item, depth int) string {
+	if len(item.Name) > 0 {
+		routes[depth] = routes[depth - 1] + "/" + item.Name
+		return routes[depth]
+	}
+
+	return routes[depth]
+}
+
+func isFile(item Item) bool {
+	return item.Type == "file"
+}
+
+func list(routes map[int]string, item Item, depth int) {
+	res, err := http.Get(getUrl(routes, item, depth))
+	if err != nil {
+		panic(err)
+	}
+
+	var data []Item
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	err = json.Unmarshal([]byte(body), &data)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Choose item (enter number):")
+	fmt.Printf("\n--------------------------------\n")
+    for i, item := range data{
+       if isFile(item) {
+       	   fmt.Printf("%d: %s (%d Mb)\n", i + 1, item.Name, item.Size / 1024 / 1024)
+       } else {
+       	   fmt.Printf("%d: %s\n", i + 1, item.Name)
+       }
+    }
+
+ 	fmt.Printf("\n--------------------------------\n")
+ 	if depth <= 0 {
+    	fmt.Printf("0: Exit\n")
+	} else {
+		fmt.Printf("0: Back\n")
+	}
+
+    var point int
+	fmt.Scanf("%d\n", &point)
+
+	if point == 0 {
+		if depth <= 0 {
+			os.Exit(0)
+		} else {
+			do(routes, Item{Name:"",Type:"",Mtime: ""}, depth - 1)
+		}
+		
+	}
+
+	if point - 1 <= len(data) {
+		do(routes, data[point - 1], depth + 1)
+	} 
+
+	do(routes, item, depth)
+}
+
+type WriteCounter struct {
+	All uint64
+	Total uint64
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+
+func (wc WriteCounter) PrintProgress() {
+	//fmt.Printf("\r%s", strings.Repeat(" ", 35))
+	fmt.Printf("\rDownloading... %d%% (%d/%d) complete", (100 * wc.Total / wc.All), wc.Total, wc.All)
+}
+
+func load(routes map[int]string, item Item, depth int) error {
+	out, err := os.Create(item.Name + ".tmp")
+	if err != nil {
+		return err
+	}
+
+	// Get the data
+	resp, err := http.Get(getUrl(routes, item, depth))
+	if err != nil {
+		out.Close()
+		return err
+	}
+	defer resp.Body.Close()
+
+	counter := &WriteCounter{All: item.Size}
+	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
+		return err
+	}
+
+	fmt.Print("\n")
+	out.Close()
+
+	if err = os.Rename(item.Name + ".tmp", item.Name); err != nil {
+		return err
+	}
+
+	return nil
+}
